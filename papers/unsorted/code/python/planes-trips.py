b538c16dea8abf92564d42ea74c0ef9d891a1c43
TK <thomas.kundera@yahoo.fr>
2016-05-11 17:03:40 +0200

    Seems OK for the flat model

--- papers/unsorted/code/python/planes-trips.py
+++ papers/unsorted/code/python/planes-trips.py
@@ -3,9 +3,9 @@
 import math, random
 from datetime import timedelta
 import scipy.stats
-import numpy # Yes, I know, I should make a choice
 import matplotlib.pyplot as plt
 
+import point
 
 kDISTDATA=[["Paris"   ,"London"     , timedelta(hours= 1,minutes=10), 200],
            ["Paris"   ,"Buccuresti" , timedelta(hours= 3,minutes= 5),1868],
@@ -13,11 +13,17 @@ kDISTDATA=[["Paris"   ,"London"     , timedelta(hours= 1,minutes=10), 200],
            ["Paris"   ,"Berlin"     , timedelta(hours= 1,minutes=45), 800],
            ["Paris"   ,"Moscow"     , timedelta(hours= 3,minutes=50),2485],
            ["Paris"   ,"HongKong"   , timedelta(hours=12,minutes=50),None],
+           ["Paris"   ,"LosAngeles" , timedelta(hours=10,minutes=35),None],
            ["HongKong","Sydney"     , timedelta(hours= 8,minutes=55),None],
            ["HongKong","LosAngeles" , timedelta(hours=13,minutes=05),None],
            ["Sydney"  ,"LosAngeles" , timedelta(hours=13,minutes=40),None],
           ]
 
+kDISTDATAtest=[["A"       ,"B"          , timedelta(hours= 1,minutes=10), 200],
+           ["A"       ,"C"          , timedelta(hours= 1,minutes=10), 300],
+           ["A"       ,"D"          , timedelta(hours= 1,minutes=10),1000],
+           ["B"       ,"C"          , timedelta(hours= 1,minutes=10), 400],
+          ]
 
 class OneFlight:
   def __init__(self,frm,to,duration,distance=None):
@@ -62,19 +68,21 @@ class AllFlights:
       if (of._distance == None):
         of._distance=self._a*of._duration.total_seconds()+self._b
 
+# Looks useless to hide _p here
+# but when going sphere, may helps to keep the
+# code generic (it is not yet)
 class FlatLocation:
   def __init__(self):
     # Random location
-    self._x=1000*random.random()-500
-    self._y=1000*random.random()-500
-    
+    self._p=point.Point((1000*random.random()-500,1000*random.random()-500))
+
   def __str__(self):
-    return "( "+str(self._x)+" , "+str(self._y)+" )"
+    return str(self._p)
 
+   
   def distance(self,other):
-    return math.sqrt( (self._x-other._x)*(self._x-other._x)
-                     +(self._y-other._y)*(self._y-other._y) )
-
+    return self._p.dist(other._p)
+  
 
 class City:
   def __init__(self,name):
@@ -84,7 +92,7 @@ class City:
     
     
   def __str__(self):
-    return self._name
+    return self._name+" "+str(self._loc)
   
   def append_distance(self,ocit,dist):
     if (not self._distances.has_key(ocit)):
@@ -94,6 +102,18 @@ class City:
 
   def distance(self,other):
     return self._loc.distance(other._loc)
+
+  def attraction_repulsion(self):
+    vtot=point.Point((0,0))
+    
+    for o,dt in self._distances.items():
+      v=o._loc._p-self._loc._p
+      d=self._loc._p.dist(o._loc._p)
+      vtot+=((d-dt)/d)*v
+      print (str(self)+" to "+str(o)+" is currently at "+str(d)
+                      +", but should be at "+str(dt)
+                      +". So moving by "+str(v)+" ( "+str(vtot)+" )")
+    self._loc._p+=.1*vtot # why .1 ? dunno
 
 
   def chi2(self):
@@ -114,8 +134,8 @@ class AllCities:
 
     for of in af._flights:
       self._citynames[of._from].append_distance(self._citynames[of._to  ],of._distance)
-      # Dont do that
-      #self._citynames[of._to  ].append_distance(self._citynames[of._from],of._distance)
+      # Have to do that too
+      self._citynames[of._to  ].append_distance(self._citynames[of._from],of._distance)
     
   def chi2(self):
     chi=0
@@ -124,23 +144,26 @@ class AllCities:
     return chi
 
   def attraction_repulsion(self):
-    pass
+    for n,c in self._citynames.items():
+      c.attraction_repulsion()
   
   
-  def display(self):
+  def display(self,stop=False):
     x=[]
     y=[]
     n=[]
     for nm,c in self._citynames.items():
       n.append(nm)
-      x.append(c._loc._x)
-      y.append(c._loc._y)
+      x.append(c._loc._p.x)
+      y.append(c._loc._p.y)
       
-      plt.annotate(nm, xy = (c._loc._x,c._loc._y ))
+      plt.annotate(nm, xy = (c._loc._p.x ,c._loc._p.y ))
       
     plt.scatter(x, y  , color='black')
-
-    plt.show()
+    if (stop):
+      plt.show()
+    else:
+      plt.draw()
     
 
 
@@ -150,10 +173,15 @@ def main():
   af.compute_missing_distances()
 
   ac=AllCities(af)
-  ac.display()
+  chi2=ac.chi2()
+  while (chi2>1000):
+    print ("Chi2= "+str(chi2))
+    #ac.display(True)
+    ac.attraction_repulsion()
+    chi2=ac.chi2()
+  ac.display(True)
+  print ("Chi2= "+str(chi2))
   
-  
-
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
   main()

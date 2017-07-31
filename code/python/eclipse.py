f7192e703263ca3ca436f734e6578b1d297ef564
TK <thomas.kundera@yahoo.fr>
2017-07-31 13:55:21 +0200

    Moon Eclips

---
+++ code/python/eclipse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+sys.path.insert(0, './common')
+import solar_system
+
+
+def main():
+  
+  
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+        main()
+
+

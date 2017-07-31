5d539a985f70d4ce5bc905736b1fc7bee5610bad
TK <thomas.kundera@yahoo.fr>
2017-07-31 15:42:55 +0200

    in progress

--- code/python/common/solar_system.py
+++ code/python/common/solar_system.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import math
+
 # Units
 # Distance
 m=1./1000.
@@ -26,17 +28,17 @@ G=6.67E-11*N*m*m/(kg*kg)
 # Nom Demi-grand axe(UA) Excentricité Inclinaison(°) Période(Y) Mass(kg) axial tilt
 
 planetsArray=[
-  ["Mercure" ,  0.38710*ua , 0.205631 ,  7.0049  ,   0.2408 *Y ,  330.2E21*kg ,-1          ],
-  ["Vénus"   ,  0.72333*ua , 0.006773 ,  3.3947  ,   0.6152 *Y , 4.8685E24*kg ,-1          ],
-  ["Terre"   ,  1.00000*ua , 0.016710 ,  0.00000 ,   1.00000*Y , 5.9736E24*kg , 23.4392811],
-  ["Mars"    ,  1.52366*ua , 0.093412 ,  1.8506  ,   1.8808 *Y , 641.85E21*kg ,-1          ],
-  ["Cérès"   ,  2.7665 *ua , 0.078375 , 10.5834  ,   4.601  *Y ,   9.46E20*kg ,-1          ],
-  ["Jupiter" ,  5.20336*ua , 0.048393 ,  1.3053  ,  11.862  *Y , 1.8986E27*kg ,-1          ],
-  ["Saturne" ,  9.53707*ua , 0.054151 ,  2.4845  ,  29.457  *Y , 568.46E24*kg ,-1          ],
-  ["Uranus"  , 19.1913 *ua , 0.047168 ,  0.7699  ,  84.018  *Y , 8.6810E25*kg ,-1          ],
-  ["Neptune" , 30.0690 *ua , 0.008586 ,  1.7692  , 164.78   *Y , 102.43E24*kg ,-1          ],
-  ["Pluton"  , 39.4817 *ua , 0.248808 , 17.1417  , 248.4    *Y ,  1.314E22*kg ,-1          ],
-  ["Éris"    , 68.1461 *ua , 0.432439 , 43.7408  , 562.55   *Y ,   1.66E22*kg ,-1          ]
+  ["Mercure" ,  0.38710*ua , 0.205631 ,  7.0049  ,   0.2408 *Y ,  330.2E21*kg ,-1          , 0],
+  ["Vénus"   ,  0.72333*ua , 0.006773 ,  3.3947  ,   0.6152 *Y , 4.8685E24*kg ,-1          , 0],
+  ["Terre"   ,  1.00000*ua , 0.016710 ,  0.00000 ,   1.00000*Y , 5.9736E24*kg , 0,0], #23.4392811 , 0],
+  ["Mars"    ,  1.52366*ua , 0.093412 ,  1.8506  ,   1.8808 *Y , 641.85E21*kg ,-1          , 0],
+  ["Cérès"   ,  2.7665 *ua , 0.078375 , 10.5834  ,   4.601  *Y ,   9.46E20*kg ,-1          , 0],
+  ["Jupiter" ,  5.20336*ua , 0.048393 ,  1.3053  ,  11.862  *Y , 1.8986E27*kg ,-1          , 0],
+  ["Saturne" ,  9.53707*ua , 0.054151 ,  2.4845  ,  29.457  *Y , 568.46E24*kg ,-1          , 0],
+  ["Uranus"  , 19.1913 *ua , 0.047168 ,  0.7699  ,  84.018  *Y , 8.6810E25*kg ,-1          , 0],
+  ["Neptune" , 30.0690 *ua , 0.008586 ,  1.7692  , 164.78   *Y , 102.43E24*kg ,-1          , 0],
+  ["Pluton"  , 39.4817 *ua , 0.248808 , 17.1417  , 248.4    *Y ,  1.314E22*kg ,-1          , 0],
+  ["Éris"    , 68.1461 *ua , 0.432439 , 43.7408  , 562.55   *Y ,   1.66E22*kg ,-1          , 0]
 ]
 sunMass=1.9891E30*kg
 
@@ -45,8 +47,21 @@ sunMass=1.9891E30*kg
 
 # Earth Moon
 # Wikipedia
-# Semi-major axis Excentricity Orbital period, inclinaison (° ecliptic)
-Earth_Moons=[384399*km, 0.0549, 27.321661*d_t, 5.145 ]
+#             Name  sma      Excentr  inclinaison (° ecliptic) Orbital period, mass tilt, offset
+Earth_Moons=["Moon",384399*km, 0.0549,  0, 27.321661*d_t, 0,0,-math.pi]
+
+
+class Point3D:
+  def __init__(self,x=0.0,y=0.0,z=0.0):
+    self.x = float(x)
+    self.y = float(y)
+    self.z = float(z)
+
+  def __add__(self, other):
+    return Point3D(self.x + other.x, self.y + other.y, self.z + other.z)
+    
+  def __str__( self ):
+    return "( "+str(self.x)+" , "+str(self.y)+" , "+str(self.z)+" )"
 
 # https://stackoverflow.com/questions/20659456/python-implementing-a-numerical-equation-solver-newton-raphson
 class NewtonRaphson:
@@ -79,7 +94,7 @@ class AnomalySolver(NewtonRaphson):
 
 
 class Orbit:
-  def __init__(self, sma, excentricity,inclinaison,period,offset=0):
+  def __init__(self, sma, excentricity,inclinaison,period,mass,tilt,offset=0):
     self.sma=sma
     self.excentricity=excentricity
     self.inclinaison=inclinaison
@@ -98,10 +113,21 @@ class Orbit:
     theta=2*math.atan(math.sqtr((1+e)*(math.tan(E/2))*(math.tan(E/2))/(1-e)))
     print (theta)
     
+class SimpleOrbit(Orbit):
+  def __init__(self, sma, excentricity,inclinaison,period,mass,tilt,offset=0):
+    super(SimpleOrbit, self).__init__(sma,excentricity,inclinaison,period,mass,tilt,offset)
+    
+  def get_location(self,t):
+    #print (self.offset)
+    theta=self.offset+t*2*math.pi/self.period
+    return Point3D(self.sma*math.cos(theta),0,self.sma*math.sin(theta))
 
 class SpaceObject:
   def __init__(self,name):
     self.name=name
+  
+  def get_location(self,t):
+    return Point3D(0,0,0)
   
   def __str__(self):
     return self.name
@@ -110,11 +136,41 @@ class Sun(SpaceObject):
   def __init__(self):
      super(Sun, self).__init__("Sun")
 
+
+class Satellite(SpaceObject):
+  def __init__(self,dataray):
+    super(Satellite, self).__init__(dataray[0])
+    self.orbit=SimpleOrbit(dataray[1],dataray[2],dataray[3],dataray[4],dataray[5],dataray[6],dataray[7])
+
+  def attach_to(self,p):
+    self.planet=p
+    
+  def get_location(self,t):
+    ploc=self.planet.get_location(t)
+    mloc=self.orbit.get_location(t)
+    #print (mloc)
+    return (mloc+ploc)
+  
+
 class SunPlanet(SpaceObject):
   def __init__(self,dataray):
     super(SunPlanet, self).__init__(dataray[0])
-    self.orbit=Orbit(dataray[1],dataray[2],dataray[3],dataray[4])
-    
+    self.orbit=SimpleOrbit(dataray[1],dataray[2],dataray[3],dataray[4],dataray[5],dataray[6],dataray[7])
+    self.sats=[]
+
+  def get_location(self,t):
+    return (self.orbit.get_location(t))
+
+  def  attach_satellite(self,sat):
+    sat.attach_to(self)
+    self.sats.append(sat)
+
+  def __str__(self):
+    s=str(self.name)+'\n'
+    for sat in self.sats:
+      s+= "    "+str(sat)
+      s+='\n'
+    return s
 
 
 class SolarSystem:
@@ -123,12 +179,15 @@ class SolarSystem:
     self.planets=[]
     for item in planetsArray:
       self.planets.append(SunPlanet(item))
+    
+    moon=Satellite(Earth_Moons)
+    self.planets[2].attach_satellite(moon)
   
   def __str__(self):
     s=""
     s+=str(self.sun)+'\n'
     for p in self.planets:
-      s+=str(p)+'\n'
+      s+=str(p)
     s+='\n'
     return s
     
--- code/python/eclipse.py
+++ code/python/eclipse.py
@@ -4,12 +4,16 @@ sys.path.insert(0, './common')
 import solar_system
 
 
+
 def main():
-  
+  ss=solar_system.SolarSystem()
+  print (ss.sun.get_location(0))
+  print (ss.planets[2].get_location(0))
+  print (ss.planets[2].sats[0].get_location(0))
   
 
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
-        main()
+  main()
 
 

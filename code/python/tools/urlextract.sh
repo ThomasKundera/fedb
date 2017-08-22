fd6d136729292bdd1ff7be0f5ff363f1840fbe51
TK <thomas.kundera@yahoo.fr>
2017-08-22 16:34:49 +0200

    Takes comment link, better than subcomment

--- code/python/tools/urlextract.sh
+++ code/python/tools/urlextract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd tmpdata
-grep 'A répondu' Historique\ -\ YouTube.html | sed -e 's#><#>@<#g' | tr '@' '\n' | grep 'A répondu' | cut -d'"' -f2  > urlist.txt 
+grep 'A répondu' Historique\ -\ YouTube.html | sed -e 's#><#>@<#g' | tr '@' '\n' | grep 'commentaire' | grep https | cut -d'"' -f2 | sort -u  > urlist.txt 
 

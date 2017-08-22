860d29f4f1c08e9db8a0eaf83fa0957067ca1833
TK <thomas.kundera@yahoo.fr>
2017-08-22 11:08:47 +0200

    doesnt work yet

--- code/python/tools/ytc.py
+++ code/python/tools/ytc.py
@@ -9,10 +9,20 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWebKitWidgets import QWebPage
 from lxml import html
 
-#Take this class for granted.Just use result of rendering.
+
+
+# From: https://impythonist.wordpress.com/2015/01/06/ultimate-guide-for-scraping-javascript-rendered-web-pages/
 class Render(QWebPage):  
   def __init__(self, url):  
-    self.app = QApplication(sys.argv)  
+    self.app = QApplication(sys.argv)
+    
+    #url = QUrl(url)
+    #username = input('username')
+    #password = input('password')
+
+    #base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
+    #authheader = "Basic %s" % base64string
+    
     QWebPage.__init__(self)  
     self.loadFinished.connect(self._loadFinished)  
     self.mainFrame().load(QUrl(url))  
@@ -22,15 +32,37 @@ class Render(QWebPage):
     self.frame = self.mainFrame()  
     self.app.quit()  
 
+class UrlList:
+  def __init__(self):
+    f=open('./tmpdata/urlist.txt')
+    
+    self.urlh={}
+    
+    for url in f.readline():
+      if (url in self.urlh):
+        print ("WARNING: duplicates: "+url)
+      else:
+        self.urlh[url]=url
+    
+  def process(self):
+    for url in self.urlh.values():
+      r = Render(url)  
+      result = r.frame.toHtml()
+      
+      
 
 
 def main():
-    url = 'http://pycoders.com/archive/'  
-    r = Render(url)  
-    result = r.frame.toHtml()
-    #This step is important.Converting QString to Ascii for lxml to process
-    archive_links = html.fromstring(str(result))
-    print (archive_links)
+  urll=UrlList()
+  
+  urll.process()
+
+  url = './tmpdata/'  
+  r = Render(url)  
+  result = r.frame.toHtml()
+  #This step is important.Converting QString to Ascii for lxml to process
+  archive_links = html.fromstring(str(result))
+  print (archive_links)
     
 # --------------------------------------------------------------------------
 if __name__ == '__main__':

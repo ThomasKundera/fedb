c57455d3885ca2d6ee1ceb3696a531c4ce21c3ef
TK <thomas.kundera@yahoo.fr>
2017-08-29 20:09:50 +0200

    better

--- code/python/tools/urlextract.sh
+++ code/python/tools/urlextract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd tmpdata
-grep 'A répondu' Historique\ -\ YouTube.html | sed -e 's#><#>@<#g' | tr '@' '\n' | grep 'commentaire' | grep https | cut -d'"' -f2 | sort -u  > urlist.txt 
+grep 'A répondu' *Historique*.html | sed -e 's#><#>@<#g' | tr '@' '\n' | grep 'commentaire' | grep https | cut -d'"' -f2 | sort -u  > urlist.txt 
 

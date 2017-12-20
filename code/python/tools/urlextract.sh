3a808d2fd63362ca21e2894c1bcf96df5e5a96ab
TK <thomas.kundera@yahoo.fr>
2017-12-20 09:04:53 +0100

    Something has change in yt html, updating

--- code/python/tools/urlextract.sh
+++ code/python/tools/urlextract.sh
@@ -2,6 +2,6 @@
 
 cd tmpdata
 grep 'A répondu' *Historique*.html | sed -e 's#><#>@<#g' | tr '@' '\n' |\
-grep 'commentaire' | tr ' ' '\n' | grep https |\
+grep 'commentaire' | tr ' ' '\n' | grep https | grep 'commentaire' |\
 cut -d'"' -f2 | sort -u  > urlist.txt 
 

22a3f7cf2856758cb6713ea56f284456944315f4
TK <thomas.kundera@yahoo.fr>
2018-01-19 10:24:56 +0100

    Also gathering notifications

--- code/python/tools/urlextract.sh
+++ code/python/tools/urlextract.sh
@@ -5,3 +5,6 @@ grep 'A répondu' *Historique*.html | sed -e 's#><#>@<#g' | tr '@' '\n' |\
 grep 'commentaire' | tr ' ' '\n' | grep https | grep 'commentaire' |\
 cut -d'"' -f2 | sort -u  > urlist.txt 
 
+grep 'ytd-notification-renderer' Historique*.html | grep href |\
+tr ' ' '\n' | grep href | cut -d'"' -f2 | grep 'lc=' | rev |\
+cut -d '.' -f2- | rev | grep 'lc=' >> urlist.txt

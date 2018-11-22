a4fe98059da919267236ed4f0cc1b69496f0d288
Thomas Kundera <thomasked69@yahoo.fr>
2018-11-22 22:03:23 +0100

    detaisl

--- code/python/tools/urlextract.sh
+++ code/python/tools/urlextract.sh
@@ -4,3 +4,4 @@ cd tmpdata
 grep 'A répondu' *Historique*.html | sed -e 's#><#>@<#g' | tr '@' '\n' |\
 grep 'commentaire' | tr ' ' '\n' | grep https | grep commentaire |\
 cut -d'"' -f2 | sort -u  > urlist.txt 
+

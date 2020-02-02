f89d7be3ce88e3e81f6e0641a341c95ea530aa0d
TK <thomas.kundera@yahoo.fr>
2020-02-02 22:52:25 +0100

    units

---
+++ code/python/common/tkunits.py
@@ -0,0 +1,39 @@
+import math
+
+# ======== MATH
+pi=math.pi
+dg=math.pi/180. # degre in radian
+
+# ======== BASE UNITS
+sr=1 # steradian
+m=1. # meter
+s=1. #second
+j=1. # Joule
+
+# ======== multiples UNITS
+# length
+# SI
+cm=m/100.
+mm=m/1000.
+km=1000.*m
+
+# Space
+ligh_year=9.4607e15*m
+au=149597870700*m
+parsec=au*648000/pi # By definition
+
+earth_radius=6371*km
+
+# Midde-age
+mile=1.60934*km
+
+# ======== derivated UNITS
+W=1*j/s           # Watt
+cd=(1./683.)*W/sr # Candela
+lm=cd*sr          # Lumen
+lx=lm/(m**2)      # Lux
+
+# ========= Physics
+eV=1.602e-19*j    # Electron-volt
+
+

d4fb6bb2a0dfdf0c96a06bb420aad89ac134392e
TK <thomas.kundera@yahoo.fr>
2020-02-24 15:05:45 +0100

    Of course a tree (not yet ok, but in progress)

--- code/python/evolution/evolution.py
+++ code/python/evolution/evolution.py
@@ -1,156 +1,94 @@
 #!/usr/bin/env python3
 import io,random
 import time
+import anytree
 
 random.seed(1)
 
-gId=0
-gMutN=0
+gId=0  # global ID
+gPop=0 # To avoid counting pop
 
 # This code is to observe distance to last common ancestor of a population
-# Likely not well wrote, however.
-
-# --------------------------------------------------------------------------
-class Mutations(object):
-  def __init__(self,m):
-    global gMutN
-    gMutN+=1
-    self.m=gMutN
-    self.mother=m
-  
-  def label(self,n):
-    # Rewrote as non recursive (recursion depth in python is limited)
-    # it's anyway most of the time faster and less memory consuming
-    try: # do not relabel
-      return(max(self.lbl,n))
-    except AttributeError:
-      self.lbl=n
-    p=self.mother
-    while(p):
-      n+=1
-      try: # do not relabel
-        return(max(p.lb,n))
-      except AttributeError:
-        p.lbl=n
-        p=p.mother
-    return(n)
-    
-  def unlabel(self):
-    p=self
-    try:
-      while (p):
-        del p.lbl
-        p=p.mother
-    except AttributeError:
-      return
-    
-  def label_check(self):
-    p=self
-    n=0
-    while (p):
-      try:
-        return(max(p.lbl,n))
-      except AttributeError:
-        p=p.mother
-        n+=1
-    
-    print("ERROR: Mutations.label_check(): unreachable statement")
-    raise
-    
-
-  # Last common ancestor
-  def lca(self,o):
-    self.label(0)
-    v=o.label_check()
-    self.unlabel() # has to be done in general
-    o.unlabel()
-    return (v)
-    
-class AncestorList(object):
-  def __init__(self,al,i):
-    # FIXME: common part of the list should be shared.
-    self.al=al
-    al.append(i)
-    
-  def lca(self,al):
-    for i in (min(len(self.al,al))):
-      if (self.al[i] != al[i]): return (len(self.al)-i)
-    return (0)
-
 
 # --------------------------------------------------------------------------
 class Idv(object):
   """Individuals of the population"""
-  def __init__(self,m):
+  def __init__(self):
+    print ("Idv: __init__(self)")
     global gId
-    #self.mut=Mutations(m)
-    self.anclist=AncestorList(m.anclist,gId)
+    global gPop
+    self.id=gId # Unique ID
     gId+=1
+    gPop+=1
     self.age=0
     self.sex=random.getrandbits(1) # not used yet
     self.deathcurve=[.5,.2,.3,.4,.5,.6,.8,1] # Likelihood of dying for each age
     self.mult=5 # How many children
-   
-  def die(self,surpop):
-   return (random.random()<self.deathcurve[self.age]*(1+surpop))
+    self.dead=False
+  
+  def onemoreyear(self,maxpop):
+    self.die(maxpop)
+    self.reproduce()
+  
+  def die(self,maxpop):
+    global gPop
+    print (self.id)
+    if (self.dead): return
+    if (random.random()<self.deathcurve[self.age]*(1+gPop/maxpop)):
+      self.dead=True
+      gPop-=1
 
-  def reproduce(self,idvl):
+  def reproduce(self):
+    if self.dead: return
     for i in range(self.mult):
-      idvl.add(Idv(self.mut))
+      c=IdvNode(parent=self)
+  
+  def __str__(self):
+    return (str(self.id))
+
+class IdvNode(Idv, anytree.NodeMixin):  # Add Node feature
+  """Individuals of the population"""
+  def __init__(self, parent=None, children=None):
+    super(IdvNode, self).__init__()
+    self.parent = parent
+    if children:  # set children only if given
+      self.children = children
 
   def __str__(self):
-    return (str(self.age))
-  
-  def lca(self,o):
-    return(self.anclist.lca(o.anclist))
-
+    return super(IdvNode, self).str()
 
 # --------------------------------------------------------------------------
 class Pop(object):
   def __init__(self):
     self.year  =  0
     self.maxpop=100. # Max number of individuals in the pop
-    self.nby   = 20  # run for that many years
+    self.nby   =  2  # run for that many years
     self.start = time.time()
 
   def doit(self):
     self.f=io.open("datafile.dat","wt")
-    self.idvl=set()
-    self.idvl.add(Idv(None))
+    self.idvtree=IdvNode()
+    
+    #n=anytree.Node('toto')
+    #for t in n.PreOrderIter():
+    #  print(t)
     
     for i in range(1,self.nby):
       if (not (i % (self.nby/10))):
         delta=int(max(1,time.time()-self.start))
         left=(delta/i)*(self.nby-i)
         print ("Year: "+str(i)+" time elaps: "+str(delta)+" time left: "+str(left))
-      self.year()
-      self.tofile()
+        print(anytree.RenderTree(self.idvtree))
+      self.doyear()
+      #self.tofile()
     self.f.close()
 
-
-  def year(self):
-    self.y=self.y+1
-    newdv=set()
-    kdv=set()
-    for idv in self.idvl:
-      if (idv.die(len(self.idvl)/self.maxpop)):
-        kdv.add(idv)
-      else:
-        idv.reproduce(newdv)
-    for idv in newdv:
-      self.idvl.add(idv)
-    for idv in kdv:
-      self.idvl.remove(idv)
-  
-  def extract_some(self,n):
-    s=[]
-    for idv in self.idvl:
-      s.append(idv)
-      n-=1
-      if (n<=0):
-        return s
-    return s # when not enouth elements
-  
+  def doyear(self):
+    self.year=self.year+1
+    for n in anytree.iterators.preorderiter.PreOrderIter(self.idvtree):
+      n.onemoreyear(self.maxpop)
+    print(anytree.RenderTree(self.idvtree))
+    
   def lca(self,s):
     # testing all agains each other is large.
     # testing just first against others
@@ -168,8 +106,6 @@ class Pop(object):
     s=self.extract_some(100)
     lca=self.lca(s)
     self.f.write(str(self.y)+" "+str(len(self.idvl))+" "+str(lca)+'\n')
-
-
 
 
 

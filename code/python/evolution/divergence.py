4764a44bb26a31262c7d4c10d4e620a862c80041
TK <thomas.kundera@yahoo.fr>
2020-09-21 11:42:58 +0200

    Maybe not so much

--- code/python/evolution/divergence.py
+++ code/python/evolution/divergence.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python 
 # FIXME: no matplotlib in python 3 for now
+
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
@@ -37,13 +39,17 @@ class Idv:
   def tick(self,wm,idvl):
     if (self.die()): return False
     self.age+=1
-    r=abs(random.gauss(0,kRadius))
-    
     
   def die(self):
     if (random.random()<Idv.deathcurve[self.age]):
       return True
     return False
+  
+  def mate(self,other):
+    self.age=0
+    self.rgb=[(self.rgb[i]+other+rgb[i])/2 for i in range(3)]
+  
+  
 
 class WorldMap:
   def __init__(self):
@@ -60,9 +66,9 @@ class WorldMap:
       self.array.append(ay)
     # Adding individuals
     for i in range(10):
-      self.addidv([i        ][i        ],Idv([1,0,0]))
-      self.addidv([kSize-1-i][i        ]=Idv([0,1,0]))
-      self.addidv([i        ][kSize-1-i]=Idv([0,0,1]))
+      self.addidv([        i,        i],Idv([1,0,0]))
+      self.addidv([kSize-1-i,        i],Idv([0,1,0]))
+      self.addidv([        i,kSize-1-i],Idv([0,0,1]))
   
   def addidv(self,xy,idv):
     self.array[xy[0]][xy[1]]=idv
@@ -72,21 +78,45 @@ class WorldMap:
     self.array[xy[0]][xy[1]]=idv
     self.idvl.append(xy)
   
+  def safeexist(ix,iy):
+    if (ix<0 or iy<0 or ix > kSize or iy > kSize): return False
+    return (self.array[ix][iy])
+
+  def lookformate(self,xy):
+    r=abs(random.gauss(0,kRadius))
+    for ix in range(-int(r)+xy[0],int(r)+xy[0]):
+      for iy in range(-int(r)+xy[1],int(r)+xy[1]):
+        if (self.safeexists(ix,iy)):
+          xym=[(xy[0]+ix)/2,(xy[1]+iy)/2]
+          if (not (self.safeexists(xym[0],iym[1]))):
+            return [ix,iy]
+    return None
+
   
   def tick(self):
     nl=[]
-    for idv in self.idvl:
-      if (not self.array[idv[0]][idv[1]].tick(self.array,self.idvl)):
-        self.array[idv[0]][idv[1]]=None
+    for xy in self.idvl:
+      # Death
+      if (not self.array[xy[0]][xy[1]].tick(self.array,self.idvl)):
+        self.array[xy[0]][xy[1]]=None
       else:
         nl.append(idv)
-        
-              
+        xy2=self.lookformate(xy)
+        if (xy2):
+          xym=[(xy[0]+xy2[0])/2,(xy[1]+xy2[1])/2]
+          self.array[xym[0]][xym[1]]=self.array[xy[0]][xy[1]].mate(self.array[xy2[0]][xy2[1]])
+          nl.append(xym)
+    self.idvl=nl
+ 
+
     
 
 class World():
   def __init__(self):
     self.wm=WorldMap()
+
+  def tick(self):
+    self.wm.tick()
 
   def draw(self):
     darray=[]
@@ -105,6 +135,7 @@ class World():
 # --------------------------------------------------------------------------
 def main():
   w=World()
+  w.tick()
   w.draw()
   
 

3e372ff8af31bf78594e362027e2499e95962859
TK <thomas.kundera@yahoo.fr>
2020-09-23 13:27:05 +0200

    Adding an image to have a more square video

--- povray/movies/sunlight/stitchimages.sh
+++ povray/movies/sunlight/stitchimages.sh
@@ -12,7 +12,9 @@ for i in `seq 365`; do
   img0=data/${i}.jpg
   img1=out1/sequence${it}.png
   img2=out2/sequence${it}.png
-  imgOUT=out3/sequence${it}.png
+  img3=out3/sequence${it}.png
+  imgOUT=out4/sequence${it}.png
 
-  montage -mode concatenate $img0 -resize 200x300 $img1 $img2 $imgOUT
+  montage -mode concatenate  -tile 2x2 $img0 -resize 200x300 $img2 $img1 $img3 $imgOUT
+  exit 0
 done

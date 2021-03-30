d104e6ad9d899edf0f59eb65ad977937f5d933e9
TK <thomas.kundera@yahoo.fr>
2021-03-30 16:25:42 +0200

    Added 2012 Blue Marble

--- povray/movies/globe-perspective-blue-marble/overlay.sh
+++ povray/movies/globe-perspective-blue-marble/overlay.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# 1972
 #convert  data/135918main_bm1_high.jpg -geometry 800x800 sequence1/sequence.png -geometry  -compose screen -composite out.jpg
-composite -geometry 900x900+150-50  data/135918main_bm1_high.jpg -compose screen  sequence1/sequence.png out.jpg
+#composite -geometry 900x900+150-50  data/135918main_bm1_high.jpg -compose screen  sequence1/sequence.png out.jpg
+
+# 2012
+composite -geometry 900x900+150-40  data/618486main_earth_full.jpg -compose screen  sequence1/sequence.png out.jpg

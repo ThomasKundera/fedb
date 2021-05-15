001a9d2c91dc8786657ae3b384ecac04926e1d14
Thomas Kundera <thomasked69@yahoo.fr>
2021-05-15 16:09:12 +0200

    Pretty close

--- papers/plane-trips/code/python/read-oneword.py
+++ papers/plane-trips/code/python/read-oneword.py
@@ -75,14 +75,18 @@ class OneWorldReader(HTMLParser):
     #if (self.state == kOWR_states.indata):
     #  print("Encountered a start tag:", tag)
     if (tag == 'div'):
+      print("------------ div -------------")
       if (self.state == kOWR_states.start):
         self.cnt+=1
         if (self.cnt == 4):
           self.state = kOWR_states.indiv
           self.cnt=0
+          self.tablefromto=[]
       else:
         self.state = kOWR_states.indiv
-    
+        self.cnt=0
+        self.tablefromto=[]
+        
   def handle_endtag(self, tag):
     #if (self.state == kOWR_states.indata):
     #  print("Encountered an end tag :", tag)
@@ -91,30 +95,32 @@ class OneWorldReader(HTMLParser):
         self.state = kOWR_states.outdiv
         self.cnt=0
         self.parse_dataline()
-        #sys.exit(0)
         
   def handle_data(self, data):
     if (self.state == kOWR_states.indiv):
       self.cnt+=1
-      if (self.cnt == 3):
-        self.cnt2=0
-        self.fromairport=Airport(data.strip())
-        #print("From airport: "+str(self.fromairport))
-      elif (self.cnt == 8):
-        self.destairport=Airport(data. split(':')[1].strip())
-        #print("Destination: "+str(self.destairport))
-      elif (data.strip() == "Aircraft time"): #self.cnt == 41):
-        self.cnt2+=1
-        if (self.cnt2==2):
-          self.state = kOWR_states.indata
-          self.cnt=self.cnt2=0
-          self.dataline=""
+      self.tablefromto.append(data.strip())
+      
+      if (data.strip() == "Aircraft time"):
+        self.state = kOWR_states.indata
+        # FIXME : we'll get junk data when 2 tables (that should be ignored hopefully)
+        print("TABLE: "+str(self.cnt))
+        print(self.tablefromto)
+        if (self.cnt>21): # Two tables
+          self.fromairport=Airport(self.tablefromto[2])
+          self.destairport=Airport(self.tablefromto[7].split(':')[1].strip())
+        else:
+          self.fromairport=Airport(self.tablefromto[2])
+          self.destairport=Airport(self.tablefromto[3].split(':')[1].strip())
+        print(self.fromairport)
+        print(self.destairport)
+        self.cnt=0
+        self.dataline=""
           #print("---------- data starts -------")
     elif (self.state == kOWR_states.indata):
       self.dataline+=" "+data.strip()
         
-    #if (self.state == kOWR_states.indata) or (self.state == kOWR_states.infillline):
-    #  print("Encountered some data  :", data)
+    print("Encountered some data  :", data)
 
   
   def parse_dataline(self):
@@ -129,22 +135,22 @@ class OneWorldReader(HTMLParser):
     # 17Nov -     12345 7 06:35 07:25   AA2940* ER4 00:50
     #     - -     12345 7 12:55 15:45   BA8217* FRJ 01:50
     print ("---------------------------------------------------------")
-    #self.dataline=" - - 12345 7 12:55 15:45 BA8217* FRJ 01:50"
     print("DATA: "+self.dataline)
+    self.dataline=re.sub('\s+', ' ', self.dataline)
     retem = re.compile(
-      "\s*(-)"+
-      "\s+(-)"+
-      "\s+([0-7\s]+)"+
-      "\s+([0-9][0-9]:[0-9][0-9])"+
-      "\s+([0-9][0-9]:[0-9][0-9])"+
-      "\s+([\w\*]+)"+
-      "\s+(\w+)"+
-      "\s+([0-9][0-9]:[0-9][0-9])"
+      "\s+(-|[0-9]{1,2}[A-Z][a-z][a-z])"+              # val1
+      "\s+(-|[0-9]{1,2}[A-Z][a-z][a-z])"+              # val2
+      "\s+([0-7\s]+)"+                                 # day
+      "\s+([0-9][0-9]:[0-9][0-9])"+                    # dep
+      "\s+([0-9][0-9]:[0-9][0-9])"+"(?:\+[0-9]+){0,1}" # arr
+      "\s+([\w\*]+)"+                                  #
+      "\s+(\w+)"+                                      #
+      "\s+([0-9][0-9]:[0-9][0-9])"                     #
       )
     items=retem.findall(self.dataline)
     s=" "
     joinitems=""
-    direct=True
+    direct=True # FIXME: this is invalid: if there are not as many from than to
     for item in items:
       joinitems+=s.join(item)+" "
       if (direct):
@@ -164,14 +170,23 @@ class OneWorldReader(HTMLParser):
     joindata=s.join(splitdata)
     #print(joindata)
     #print(joinitems)
-    df=""
-    for i,s in enumerate(difflib.ndiff(joindata,joinitems)):
-        if s[0]==' ': continue
-        elif s[0]=='-':
-            df+=s[-1]
-    print ("ERROR: "+df)
-    if ("-" in df):
-      sys.exit(0)
+    matches = difflib.SequenceMatcher(None,joindata , joinitems).get_matching_blocks()
+    ptr=0
+    for match in matches:
+      print("Matching: "+str(match.a)+" "+str(match.a+match.size))
+      if ("-" in joindata[ptr:match.a]):
+        print("ERROR: "+str(ptr)+" "+str(match.a))
+        print(joindata[ptr:match.a])
+        print("--")
+        print(joindata)
+        print("--")
+        print(joinitems)
+        print("----")
+        for item in items:
+          print (item)
+        #sys.exit(0) FIXME: errors shouldn't be ignored
+      ptr=match.a + match.size
+    
     
 
 def main():

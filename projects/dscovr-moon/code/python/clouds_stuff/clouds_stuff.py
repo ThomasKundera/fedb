b18434091eeedb8173ac1f86dcb747bdc6bd4ed3
Thomas Kundera <thomasked69@yahoo.fr>
2022-09-06 19:45:16 +0200

    Finally: clouds

---
+++ projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data, color
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.feature import canny
+from skimage.draw import circle_perimeter
+from skimage.util import img_as_ubyte
+
+
+# Load picture and detect edges
+image = img_as_ubyte(data.coins()[160:230, 70:270])
+edges = canny(image, sigma=3, low_threshold=10, high_threshold=50)
+
+
+# Detect two radii
+hough_radii = np.arange(20, 35, 2)
+hough_res = hough_circle(edges, hough_radii)
+
+# Select the most prominent 3 circles
+accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,
+                                           total_num_peaks=3)
+
+# Draw them
+fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
+image = color.gray2rgb(image)
+for center_y, center_x, radius in zip(cy, cx, radii):
+    circy, circx = circle_perimeter(center_y, center_x, radius,
+                                    shape=image.shape)
+    image[circy, circx] = (220, 20, 20)
+
+ax.imshow(image, cmap=plt.cm.gray)
+plt.show()
+

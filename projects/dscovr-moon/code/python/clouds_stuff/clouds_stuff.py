f89e4fa91c442f39898091884723253b8e028cfd
Thomas Kundera <thomasked69@yahoo.fr>
2022-09-06 21:52:05 +0200

    First part done

--- projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
+++ projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
@@ -11,31 +11,56 @@ from skimage.draw import circle_perimeter, set_color
 from skimage.util import img_as_ubyte
 
 
-# Load picture and detect edges
-image0  = io.imread('data/EPIC_00000.png', as_gray=True)
-imagergb0 = io.imread('data/EPIC_00000.png')
+class Clouding:
+  def __init__(self,fn,rscl=1):
+    self._fn=fn
+    self._rescale=rscl # Rescale factor. 1 means 100%
 
-image = rescale(image0, 0.25, anti_aliasing=False)
-imagergb = rescale(imagergb0, 0.25, multichannel=True, anti_aliasing=False)
+    # Load picture and detect edges
+    img0    = io.imread(self._fn, as_gray=True)
+    imgRGB0 = io.imread(self._fn)
 
-#image = img_as_ubyte(data.coins()[160:230, 70:270])
-edges = canny(image)
+    self._img    = rescale(img0   , self._rescale, anti_aliasing=True                   )
+    self._imgRGB = rescale(imgRGB0, self._rescale, anti_aliasing=True, multichannel=True)
+    
+  def findEdges(self):
+    self._edges = canny(self._img)
+    
+  def findCircle(self):
+    # Detect two radii
+    hough_radii = np.arange(int(self._rescale*800), int(self._rescale*1000), 2)
+    hough_res = hough_circle(self._edges, hough_radii)
+    # Select the most prominent 1 circles
+    self._accums, self._cx, self._cy, self._radii = hough_circle_peaks(hough_res, hough_radii,total_num_peaks=1)
 
-# Detect two radii
-hough_radii = np.arange(int(0.25*800), int(0.25*1000), 2)
-hough_res = hough_circle(edges, hough_radii)
+  def draw(self):
+    # Draw them
+    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
+    for center_y, center_x, radius in zip(self._cy, self._cx, self._radii):
+      print ("( "+str(center_x)+" , "+str(center_y)+" , "+str(radius)+" ) ")
+      circy, circx = circle_perimeter(center_y, center_x, radius, shape=self._imgRGB.shape)
+      set_color(self._imgRGB, (circy, circx) , [250, 50, 50])
 
-# Select the most prominent 1 circles
-accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,total_num_peaks=1)
+    ax.imshow(self._imgRGB, cmap=plt.cm.gray)
+    plt.show()
+  
+    
+  
+    
+  def process(self):
+    self.findEdges()
+    self.findCircle()
+    self.draw()
+    
 
-# Draw them
-fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
-image = color.gray2rgb(image)
-for center_y, center_x, radius in zip(cy, cx, radii):
-    print ("( "+str(center_x)+" , "+str(center_y)+" , "+str(radius)+" ) ")
-    circy, circx = circle_perimeter(center_y, center_x, radius, shape=imagergb.shape)
-    set_color(imagergb, (circy, circx) , [250, 50, 50])
 
-ax.imshow(imagergb, cmap=plt.cm.gray)
-plt.show()
+def main():
+  cl=Clouding("data/EPIC_00000.png",0.1)
+  cl.process()
+
+
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
 

4c4dcd8a403d740764607209bd66c1226ca8a3d8
Thomas Kundera <thomasked69@yahoo.fr>
2022-09-06 21:38:15 +0200

    Find circle test seems OK

--- projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
+++ projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
@@ -5,33 +5,37 @@ import matplotlib.pyplot as plt
 
 from skimage import io
 from skimage import data, color
-from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.transform import hough_circle, hough_circle_peaks, rescale
 from skimage.feature import canny
-from skimage.draw import circle_perimeter
+from skimage.draw import circle_perimeter, set_color
 from skimage.util import img_as_ubyte
 
 
 # Load picture and detect edges
-image  = io.imread('EPIC_00000-tiny.png', as_gray=True)
-image0 = io.imread('EPIC_00000-tiny.png')
+image0  = io.imread('data/EPIC_00000.png', as_gray=True)
+imagergb0 = io.imread('data/EPIC_00000.png')
+
+image = rescale(image0, 0.25, anti_aliasing=False)
+imagergb = rescale(imagergb0, 0.25, multichannel=True, anti_aliasing=False)
+
 #image = img_as_ubyte(data.coins()[160:230, 70:270])
 edges = canny(image)
 
 # Detect two radii
-hough_radii = np.arange(20, 35, 2)
+hough_radii = np.arange(int(0.25*800), int(0.25*1000), 2)
 hough_res = hough_circle(edges, hough_radii)
 
 # Select the most prominent 1 circles
-accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,total_num_peaks=3)
+accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,total_num_peaks=1)
 
 # Draw them
 fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
 image = color.gray2rgb(image)
 for center_y, center_x, radius in zip(cy, cx, radii):
     print ("( "+str(center_x)+" , "+str(center_y)+" , "+str(radius)+" ) ")
-    circy, circx = circle_perimeter(center_y, center_x, radius, shape=image.shape)
-    image[circy, circx] = (220, 20, 20)
+    circy, circx = circle_perimeter(center_y, center_x, radius, shape=imagergb.shape)
+    set_color(imagergb, (circy, circx) , [250, 50, 50])
 
-ax.imshow(image, cmap=plt.cm.gray)
+ax.imshow(imagergb, cmap=plt.cm.gray)
 plt.show()
 

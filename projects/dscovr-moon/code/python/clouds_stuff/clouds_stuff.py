1c73dec22e57fc7ac148013d00a158f8788be10b
Thomas Kundera <thomasked69@yahoo.fr>
2022-09-06 20:27:17 +0200

    Works a bit, in progress

--- projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
+++ projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from skimage import io
 from skimage import data, color
 from skimage.transform import hough_circle, hough_circle_peaks
 from skimage.feature import canny
@@ -11,24 +12,24 @@ from skimage.util import img_as_ubyte
 
 
 # Load picture and detect edges
-image = img_as_ubyte(data.coins()[160:230, 70:270])
-edges = canny(image, sigma=3, low_threshold=10, high_threshold=50)
-
+image  = io.imread('EPIC_00000-tiny.png', as_gray=True)
+image0 = io.imread('EPIC_00000-tiny.png')
+#image = img_as_ubyte(data.coins()[160:230, 70:270])
+edges = canny(image)
 
 # Detect two radii
 hough_radii = np.arange(20, 35, 2)
 hough_res = hough_circle(edges, hough_radii)
 
-# Select the most prominent 3 circles
-accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,
-                                           total_num_peaks=3)
+# Select the most prominent 1 circles
+accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,total_num_peaks=3)
 
 # Draw them
 fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
 image = color.gray2rgb(image)
 for center_y, center_x, radius in zip(cy, cx, radii):
-    circy, circx = circle_perimeter(center_y, center_x, radius,
-                                    shape=image.shape)
+    print ("( "+str(center_x)+" , "+str(center_y)+" , "+str(radius)+" ) ")
+    circy, circx = circle_perimeter(center_y, center_x, radius, shape=image.shape)
     image[circy, circx] = (220, 20, 20)
 
 ax.imshow(image, cmap=plt.cm.gray)

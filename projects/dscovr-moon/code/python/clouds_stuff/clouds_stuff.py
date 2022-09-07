1621e0e97744f504c937940f41752d11e95745f8
TK <thomas.kundera@yahoo.fr>
2022-09-07 15:54:52 +0200

    Lots of cleanup

--- projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
+++ projects/dscovr-moon/code/python/clouds_stuff/clouds_stuff.py
@@ -43,8 +43,8 @@ class Clouding:
     print("Clouding.findCircle(): START")
     # Detect two radii
     hough_radii = np.arange(
-      int(self._edgrescale*self._rescale*800), 
-      int(self._edgrescale*self._rescale*1000), 2)
+      int(self._edgrescale*self._rescale*850), 
+      int(self._edgrescale*self._rescale*900), 2)
     hough_res = hough_circle(self._edges, hough_radii)
     # Select the most prominent 1 circles
     # The x/y inversion here seems needed.
@@ -64,14 +64,7 @@ class Clouding:
     for center_y, center_x, radius in zip(self._cy, self._cx, self._radii):
       print ("( "+str(center_x)+" , "+str(center_y)+" , "+str(radius)+" ) ")
       circx, circy = circle_perimeter(center_x, center_y, radius, shape=self._imgRGB.shape)
-      #set_color(self._imgRGBcircle, (circx, circy) , [250, 50, 50])
       self._imgRGBcircle[circx,circy]=(.8, .2, .2)
-      #print (self._imgRGBcircle[center_x,center_y])
-      #print (self._imgRGBcircle[center_x+1,center_y+1])
-      #self._imgRGBcircle[center_x,center_y]=(.8, .2, .2)
-      #print (self._imgRGBcircle[center_x+1,center_y+1])
-      #print (self._imgRGBcircle[center_x,center_y])
-    #self._imgRGBcircle=self._imgRGBcircle.copy()
     print("Clouding.addCircle(): END")
       
     
@@ -96,121 +89,6 @@ class Clouding:
     self._radii=[int(len(self._img   )/2.5)]
     print ("( "+str(self._cx[0])+" , "+str(self._cy[0])+" , "+str(self._radii[0])+" ) ")
 
-
-  def processFlattenForward(self):
-    self._flatten_image=0*self._imgRGB
-    self._flatten_image=rescale(self._flatten_image,1.5, multichannel=True)
-    cx=self._cx[0]
-    cy=self._cy[0]
-    r0=self._radii[0]
-    
-    # FIXME: there is some weird swapping of x/y coordinates
-    # we'll deal with it for now.
-    ym=cx-r0
-    yM=cx+r0
-    xm=cy-r0
-    xM=cy+r0
-
-    print ("Circle : ( "+str(cx)+" , "+str(cy)+" ) "+str(r0))
-    for ix in range(xm,xM):
-      for iy in range(ym,yM):
-        #print("( "+str(ix)+" , " +str(iy)+" )")
-        try:
-          color=self._imgRGB[ix,iy]
-          #print(color)
-        except IndexError:
-          print("That should not happens: ( "+str(int(ix))+" , " +str(int(iy))+" )")
-          continue
-        
-        r=math.sqrt((cy-ix)*(cy-ix)+(cx-iy)*(cx-iy))
-        theta=math.atan2(iy-cx,ix-cy)
-        #print ("theta: "+str(theta*180/math.pi)+" x: "+str(ix-cx)+" y: "+str(iy-cy))
-        #print ("r0="+str(r0)+" r="+str(r))
-        
-        try:
-          alpha=math.asin(r/r0)
-        except ValueError:
-          #print("out of the circle: ( "+str(int(ix))+" , " +str(int(iy))+" )")
-          continue
-        
-        #print ("alpha: "+str(alpha*180/math.pi))
-        r1=r0*alpha
-        
-        x=r1*math.cos(theta)+len(self._flatten_image)/2
-        y=r1*math.sin(theta)+len(self._flatten_image[0])/2
-        
-        try:
-          self._flatten_image[int(x),int(y)]=color
-          #print("In range: ( "+str(int(x))+" , " +str(int(y))+" )")
-        except IndexError:
-         #print("Out of range: ( "+str(int(x))+" , " +str(int(y))+" )")
-         continue
-    io.imsave("toto.png",self._imgRGB)
-    io.imsave("titi.png",self._flatten_image)
-
-
-
-
-  def flattening_color(self,x0,y0):
-    cx0=self._cx[0]
-    cy0=self._cy[0]
-    r0=math.sqrt((x0-cx0)*(x0-cx0)+(y0-cy0)*(y0-cy0))
-    theta0=math.atan2(y0-cy0,x0-cx0)
-    
-    cx=len(self._flatten_image)/2
-    cy=len(self._flatten_image[0])/2
-    
-    #theta=math.atan2(y-cy,x-cx)
-    
-    x2=cx+r0*math.cos(theta0)
-    y2=cx+r0*math.sin(theta0)
-    
-    
-    try:
-      color=self._imgRGB[int(x2),int(y2)]
-      if (1): # np.linalg.norm(color) > 0.1 ):
-        print("( "+str(x0)+" , "+str(y0)+" ) => ( "+str(x2)+" , "+str(y2)+" ) = ("+str(r0)+" , "+str(theta0*180/math.pi)+") = "+str(color))
-      return self._imgRGB[int(x2),int(y2)]
-    except IndexError:
-      return [0,0,0]
-
-  def processFlattenBackward(self):
-    self._flatten_image=0*self._imgRGB
-    self._flatten_image=rescale(self._flatten_image,1.5, multichannel=True)
-    
-    for ix in range(len(self._flatten_image)):
-      for iy in range(len(self._flatten_image[0])):
-        self._flatten_image[ix,iy]=self.flattening_color(ix,iy)
-        
-    
-    io.imsave("toto.png",self._imgRGB)
-    io.imsave("titi.png",self._flatten_image)
-
-  
-  def find_forward_spherical(self,x,y):
-    cx=self._cx[0]
-    cy=self._cy[0]
-    theta=math.atan2(y-cy,x-cx)
-    
-    r=math.sqrt((x-cx)*(x-cx)+(y-cy)*(y-cy))
-    theta=math.atan2(y-cy,x-cx)
-    
-    
-    return (x,y)
-   
-
-  def processFlattenForwardSpherical(self):
-    self._flatten_image=0*self._imgRGB
-    self._flatten_image=rescale(self._flatten_image,1.5, multichannel=True)
-    
-    for ix in range(len(self._imgRGB)):
-      for iy in range(len(self._imgRGB[0])):
-        (theta,phi)=self.find_forward_spherical(ix,iy)
-        self._flatten_image[theta,phi]=self._imgRGB[ix,iy]
-        
-    
-    io.imsave("toto.png",self._imgRGB)
-    io.imsave("titi.png",self._flatten_image)
     
   # Wikipedia c'est la vie!
   # https://en.wikipedia.org/wiki/Orthographic_map_projection#Mathematics

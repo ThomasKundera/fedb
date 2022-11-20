882a608523919f99aae2bab77e51b1be32e5b661
Thomas Kundera <thomasked69@yahoo.fr>
2022-11-20 10:16:32 +0100

    done

--- projects/dscovr-moon/data/one_year_epic/sum-animate.sh
+++ projects/dscovr-moon/data/one_year_epic/sum-animate.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-for img in `ls ../large_processed/*.png`; do
-  imgo=`basename $img`
-  convert $img  -background "#000000" -resize "1920x1080" -rotate 26.2 -crop "1920x1080+10+4" -resize "1925x1085!" -crop "1920x1080+8+6" $imgo
+imglist="00000@00066 00058@00076 00087@00086 00116@00096 00145@00106 00174@00116 00203@00126 00232@00136 00261@00146 00290@00156 00319@00166 00348@00176 00377@00186 00435@00206 00464@00216 00493@00226 00522@00236 00551@00246"
+
+i=0
+for imgc in $imglist; do
+  imga=`echo $imgc | cut -d'@' -f1`
+  imga=EPIC_${imga}.png
+  imgb=`echo $imgc | cut -d'@' -f2`
+  imgb=extract${imgb}.png
+  echo $imgc $imga $imgb
+  is=$i
+  if [ $i -lt 10 ]; then is=0${i}; fi
+    
+  montage $imga $imgb -tile 1x2 -geometry 455x256+1+1 tiled${is}.png
+  ((i++))
 done
 
 

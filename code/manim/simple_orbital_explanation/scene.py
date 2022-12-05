1fa07fb25956d4d6808762113a47d5641dd09bd2
TK <thomas.kundera@yahoo.fr>
2022-12-05 21:53:30 +0100

    Continuying

--- code/manim/simple_orbital_explanation/scene.py
+++ code/manim/simple_orbital_explanation/scene.py
@@ -25,24 +25,87 @@ class Momentum(Scene):
         self.wait(3)
         self.remove(text)
         
-        text = Text(
-          '... We have an object moving with some speed...',
-          slant=ITALIC).scale(.5).to_edge(DL)
-        dot = Dot(plane.coords_to_point(0,14), color=GREEN)
-        a1 = Arrow(
-          plane.coords_to_point(0,14),
-          plane.coords_to_point(2,14),
-          color=BLUE)
-        self.add(text,a1,dot)
-        
-        l1 = Line (plane.coords_to_point(0,14),plane.coords_to_point(2,14))
-        
-        self.add(plane) #, dot_scene, ax, dot_axes, lines)
+        txtstr=[
+          'We have an object moving with some speed',
+          'advancing 2 units per unit of time',
+          'without any force acting on it.',
+          'That object will continue at same speed',
+          'on a straight line.'
+          ]
         
         for i in range(10):
+          txt_group = VGroup()
+          for j in range(0,min(i,5)):
+            text = Text(txtstr[j],slant=ITALIC).scale(.8)
+            txt_group += text
+          txt_group.arrange(DOWN).to_edge(DL)
           dot = Dot(plane.coords_to_point(2*i,14), color=GREEN)
           l1 = Line(plane.coords_to_point(2*i,14),plane.coords_to_point(2*(i+1),14))
-          self.play(MoveAlongPath(dot, l1), rate_func=linear, run_time=1)
+          a1 = Arrow(
+            plane.coords_to_point(2*i,14),
+            plane.coords_to_point(2*(i+1),14),
+            color=BLUE)
+          self.add(txt_group,a1,dot)
+          self.play(MoveAlongPath(dot, l1), rate_func=linear, run_time=2)
+          self.remove(txt_group)
+        self.wait(1)
         
         
+        
+class FlatGravity(Scene):
+  
+    def myopenning(self):
+        text = Text('Trajectories on flat Earth').scale(1.1)
+        self.add(text)
+        self.wait(3)
+        self.remove(text)
+    
+    def construct(self):
+        self.myopenning()
+        plane = NumberPlane(
+          x_range=(-2, 28, 2),
+          y_range=(-2, 18, 2),
+          x_length=15, 
+          y_length=9)
+        plane.add_coordinates()
+        text = Text(
+          '... Lets suppose a constant gravitational field...',
+          slant=ITALIC).scale(1).to_edge(DL)
+        
+        self.add(plane)
+        self.add(text)
+        self.wait(3)
+        self.remove(text)
+        
+        txtstr=[
+          'At small scale, on Earth everything happens',
+          'like Earth would be flat',
+          'and gravitation would be constant.',
+          'Newtonnian gravity acts like a force',
+          '(called weight), and thus',
+          'is accelerating things down,',
+          'so speed increasse with time.',
+          ]
+        y=14
+        v=0
+        a=-1
+        
+        for i in range(8):
+          txt_group = VGroup()
+          for j in range(0,min(i,len(txtstr))):
+            text = Text(txtstr[j],slant=ITALIC).scale(.7)
+            txt_group += text
+          txt_group.arrange(DOWN).to_edge(DR)
+          dot = Dot(plane.coords_to_point(2,y), color=GREEN)
+          l1 = Line(plane.coords_to_point(2,y),plane.coords_to_point(2,y+v))
+          a1 = Arrow(
+            plane.coords_to_point(2,y),
+            plane.coords_to_point(2,y+v),
+            color=BLUE)
+          self.add(txt_group,a1,dot)
+          self.play(MoveAlongPath(dot, l1), rate_func=linear, run_time=2)
+          self.remove(txt_group)
+          y=y+v
+          v=v+a
+        self.wait(1)
         

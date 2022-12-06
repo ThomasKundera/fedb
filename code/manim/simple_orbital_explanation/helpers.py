d72c967cccc68cf33d5376ae000ba7c5093ea5e1
TK <thomas.kundera@yahoo.fr>
2022-12-06 13:29:02 +0100

    Some test work

--- code/manim/simple_orbital_explanation/helpers.py
+++ code/manim/simple_orbital_explanation/helpers.py
@@ -26,29 +26,57 @@ class Point2:
     d=n.norm()
     return d*d*d
   
+  def tpl(self):
+    return [self._x,self._y,0]
+  
+  def __str__(self):
+    return '( '+str(self._x)+' , '+str(self._y)+' )'
+  
 class Xva:
   def __init__(self,x=Point2(),v=Point2(),a=Point2()):
+    print("Xva.__int__()")
     self._x=x
     self._v=v
     self._a=a
 
-  def do_it(self):
-    dot = Dot((p._x,p._y), color=GREEN)
-    a1 = Arrow(
-      (p._x, p._y),
-      plane.coords_to_point(pn._x,pn._y),
-      color=BLUE)
-    l1 = Line(p._x,p._y,p._x+v._x,p._y+v._y)
+  def do_it(self,scene):
+    print("Xva.do_it()")
+    nx=self._x+self._v+self._a
+    nv=self._v+self._a
+    print(self._x.tpl())
+    p0 = Dot(self._x.tpl(), color=GREEN)
+    print(p0)
+    v0 = Arrow( self._x.tpl()         ,(self._x+self._v).tpl(), buff=0.1, color=BLUE_A)
+    v1 = Arrow( self._x.tpl()         ,(self._x+nv).tpl()     , buff=0.1, color=BLUE_C)
+    a  = Arrow((self._x+self._v).tpl(),(self._x+nv).tpl()     , buff=0.1, color=RED_C )
+    l1 = Line(self._x.tpl(),nx.tpl())
+
+    scene.add(p0)
+    scene.wait(1)
+    scene.add(v0)
+    scene.wait(1)
+    scene.add(a)
+    scene.wait(1)
+    scene.add(v1)
+    scene.wait(1)
+    scene.play(MoveAlongPath(p0, l1), rate_func=linear, run_time=2)
+    
+    return (p0,v0,a,nv)
     
 
 class TestMe(Scene):
   def construct(self):
-    self.myopenning()
     plane = NumberPlane(
       x_range=(-15, 15, 2),
       y_range=(-10, 10, 2),
       x_length=15, 
       y_length=9)
     plane.add_coordinates()
-    
+    self.add(plane)
+    x=Point2(2,2)
+    v=Point2(3,2)
+    a=Point2(0,-1)
+    xva=Xva(x,v,a)
+    (p0,v0,a,nv)=xva.do_it(self)
+    print(p0)
     

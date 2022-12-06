2b79b33313fe7b499cabb6ffa437fbdac8ed6e83
TK <thomas.kundera@yahoo.fr>
2022-12-06 13:43:43 +0100

    Somewhat better

--- code/manim/simple_orbital_explanation/helpers.py
+++ code/manim/simple_orbital_explanation/helpers.py
@@ -29,46 +29,50 @@ class Point2:
   def tpl(self):
     return [self._x,self._y,0]
   
+  def ipl(self,plane):
+    return plane.coords_to_point(self._x,self._y)
+  
   def __str__(self):
     return '( '+str(self._x)+' , '+str(self._y)+' )'
   
 class Xva:
   def __init__(self,x=Point2(),v=Point2(),a=Point2()):
-    print("Xva.__int__()")
+    #print("Xva.__int__()")
     self._x=x
     self._v=v
     self._a=a
 
-  def do_it(self,scene):
-    print("Xva.do_it()")
-    nx=self._x+self._v+self._a
-    nv=self._v+self._a
-    print(self._x.tpl())
-    p0 = Dot(self._x.tpl(), color=GREEN)
-    print(p0)
-    v0 = Arrow( self._x.tpl()         ,(self._x+self._v).tpl(), buff=0.1, color=BLUE_A)
-    v1 = Arrow( self._x.tpl()         ,(self._x+nv).tpl()     , buff=0.1, color=BLUE_C)
-    a  = Arrow((self._x+self._v).tpl(),(self._x+nv).tpl()     , buff=0.1, color=RED_C )
-    l1 = Line(self._x.tpl(),nx.tpl())
+  def do_it(self,scene,pl):
+    #print("Xva.do_it()")
+    x=self._x
+    v=self._v
+    a=self._a
+    nx=x+v+a
+    nv=v+a
+    p0 = Dot(x.ipl(pl), color=GREEN)
+    v0 = Arrow( x.ipl(pl)         ,(x+v ).ipl(pl)     , buff=0.1, color=BLUE_A)
+    v1 = Arrow( x.ipl(pl)         ,(x+nv).ipl(pl)     , buff=0.1, color=BLUE_C)
+    a1 = Arrow((x+v).ipl(pl)      ,(x+nv).ipl(pl)     , buff=0.1, color=RED_C )
+    l1 = Line(self._x.ipl(pl),nx.ipl(pl))
 
     scene.add(p0)
     scene.wait(1)
     scene.add(v0)
     scene.wait(1)
-    scene.add(a)
+    scene.add(a1)
     scene.wait(1)
     scene.add(v1)
     scene.wait(1)
     scene.play(MoveAlongPath(p0, l1), rate_func=linear, run_time=2)
-    
+    scene.wait(5)    
     return (p0,v0,a,nv)
     
 
 class TestMe(Scene):
   def construct(self):
     plane = NumberPlane(
-      x_range=(-15, 15, 2),
-      y_range=(-10, 10, 2),
+      x_range=(-2, 28, 2),
+      y_range=(-2, 18, 2),
       x_length=15, 
       y_length=9)
     plane.add_coordinates()
@@ -77,6 +81,6 @@ class TestMe(Scene):
     v=Point2(3,2)
     a=Point2(0,-1)
     xva=Xva(x,v,a)
-    (p0,v0,a,nv)=xva.do_it(self)
+    (p0,v0,a,nv)=xva.do_it(self,plane)
     print(p0)
     

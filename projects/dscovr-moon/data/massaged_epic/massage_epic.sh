8aa52a56fb564ed9f6c200f3cd2d6b8c3bef955b
Thomas Kundera <thomasked69@yahoo.fr>
2022-12-23 08:41:18 +0100

    new value

--- projects/dscovr-moon/data/massaged_epic/massage_epic.sh
+++ projects/dscovr-moon/data/massaged_epic/massage_epic.sh
@@ -3,6 +3,6 @@
 for imgfp in `ls ../large_processed/*.png`; do
   img=`basename $imgfp`
   echo "Processing $img"
-  convert $imgfp -rotate 35.15 -crop 2000x2000+1000+100    $img
+  convert $imgfp -rotate 25 -crop 2000x2000+1000+100    $img
 done
 

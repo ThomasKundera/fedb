b3dfcefb1d43198f4f8a8de7b6f130e8a22f2ca5
TK <thomas.kundera@yahoo.fr>
2022-12-24 10:37:59 +0100

    seems OK

--- code/python/divers/atmosphere_density.py
+++ code/python/divers/atmosphere_density.py
@@ -9,22 +9,38 @@ import sys
 sys.path.insert(0, '../common')
 
 import math
-from tkunits import cm, km, earth_radius
+from tkunits import kPa, K, Na, R, cm, m, km, earth_radius
 
 # By fitting of the curve on Figure 10 by a 1/r³ function
 # we get that constant:
 k=30.**3*earth_radius**3
 
 def fdensity(r):
-    return(k/r**3)
+  return(k/r**3)
+
+# The value is in molecule per m⁻³
+# So the conversion factor from cm⁻³
+def Fdensity(r):
+  return (-k/(2*r**2))/(cm**3)
 
 
-def main():
+def test_fit():
   for dr in [3,10,20,30,40,50,100]:
     d=dr*earth_radius
     print("f( "+str(d)+" ) = "+str(fdensity(d)))
 
-    F=-3
+
+def main():
+  Nb=Fdensity(100*earth_radius)-Fdensity(3*earth_radius)
+  
+  # PV=nRT
+  P=100*kPa
+  V=1*m**3
+  T=300*K
+  n=Na*(P*V/(R*T))
+  
+  print(Nb/n)
+  
   
 # --------------------------------------------------------------------------
 if __name__ == '__main__':

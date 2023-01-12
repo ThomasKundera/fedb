7d128f8201e233c2f9bb62325f554d06f98041f8
TK <thomas.kundera@yahoo.fr>
2023-01-12 10:19:24 +0100

    Added a few units, mainlky for thermoedynamics

--- code/python/common/tkunits.py
+++ code/python/common/tkunits.py
@@ -5,10 +5,25 @@ pi=math.pi
 dg=math.pi/180. # degre in radian
 
 # ======== BASE UNITS
-sr=1 # steradian
-m=1. # meter
-s=1. #second
-j=1. # Joule
+sr=1  # steradian
+m=1.  # meter
+s=1.  # second
+J=1.  # Joule
+kg=1. # Kilogram
+
+K=1.   # Kelvin
+mol=1. # Mole
+
+Na=6.02214076*10**23 # Avogadro Number
+
+# ======== derivated UNITS
+W=1*J/s           # Watt
+cd=(1./683.)*W/sr # Candela
+lm=cd*sr          # Lumen
+lx=lm/(m**2)      # Lux
+N=kg*m*s**(-2)    # Newton
+Pa=N/m**2         # Pascal
+
 
 # ======== multiples UNITS
 # length
@@ -16,6 +31,8 @@ j=1. # Joule
 cm=m/100.
 mm=m/1000.
 km=1000.*m
+
+kPa=1000*Pa
 
 # Space
 ligh_year=9.4607e15*m
@@ -27,14 +44,13 @@ earth_radius=6371*km
 # Midde-age
 mile=1.60934*km
 
-# ======== derivated UNITS
-W=1*j/s           # Watt
-cd=(1./683.)*W/sr # Candela
-lm=cd*sr          # Lumen
-lx=lm/(m**2)      # Lux
+
 
 # ========= Physics
-eV=1.602e-19*j    # Electron-volt
+eV=1.602e-19*J    # Electron-volt
+
+
+R=8.31446261815324*J*mol**(-1)*K**(-1)
 
 # Local
 g_earth=9.81*m/(s*s)

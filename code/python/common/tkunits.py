d38ef9e96d27bc6680ff032b818f6fcf0ceb9ffb
TK <thomas.kundera@yahoo.fr>
2023-01-20 15:17:17 +0100

    Some added units

--- code/python/common/tkunits.py
+++ code/python/common/tkunits.py
@@ -32,6 +32,9 @@ cm=m/100.
 mm=m/1000.
 km=1000.*m
 
+# mass
+g=kg/1000.
+
 kPa=1000*Pa
 
 # Space
@@ -40,15 +43,23 @@ au=149597870700*m
 parsec=au*648000/pi # By definition
 
 earth_radius=6371*km
+earth_mass=5.972e24*kg
+
 
 # Midde-age
 mile=1.60934*km
 
-
+# ======== derivated UNITS
+J=1*kg*m*m/(s*s)
+N=1*kg*m/(s*s)
+W=1*J/s           # Watt
+cd=(1./683.)*W/sr # Candela
+lm=cd*sr          # Lumen
+lx=lm/(m**2)      # Lux
 
 # ========= Physics
 eV=1.602e-19*J    # Electron-volt
-
+gravitational_constant=6.6742e-11*N*m*m/(kg*kg)
 
 R=8.31446261815324*J*mol**(-1)*K**(-1)
 

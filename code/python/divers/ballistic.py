377498e5045c898115542587c698e76fde0bf3d8
TK <thomas.kundera@yahoo.fr>
2023-01-20 15:19:09 +0100

    maybe one day

---
+++ code/python/divers/ballistic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Simulation of ballistic trajectory on 
+# - flat plane
+# - globe
+# - concave Earth
+# Taking into account:
+# - atmosphere (and atmospheric gradiant)
+# Ignoring:
+# - Earth self-rotation
+
+
+import sys
+sys.path.insert(0, '../common')
+
+import math
+from tkunits import km, dg, pi, earth_radius
+
+def main():
+  
+  
+  
+  
+  
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
+
+
+

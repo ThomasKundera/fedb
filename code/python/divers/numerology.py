d6c2ad6f8a59ad1c163711f115c3c4f5a3b992fc
TK <thomas.kundera@yahoo.fr>
2023-01-20 15:19:32 +0100

    progress

--- code/python/divers/numerology.py
+++ code/python/divers/numerology.py
@@ -4,15 +4,33 @@ import sys
 name="excite".lower()
 
 
-for c in name:
-  print (c+" "+str(ord(c)-ord('a')+1))
+#for c in name:
+  #print (c+" "+str(ord(c)-ord('a')+1))
 
-sys.exit(0)
+#sys.exit(0)
 
-def sumchar(w):
+def sumchar1(w):
   s=0
   for c in w:
     s+=ord(c)-ord('a')+1
+  return s
+
+def sumchar0(w):
+  s=0
+  for c in w:
+    s+=ord(c)-ord('a')
+  return s
+
+def revsumchar1(w):
+  s=0
+  for c in w:
+    s+=ord('z')-ord(c)+1
+  return s
+
+def revsumchar0(w):
+  s=0
+  for c in w:
+    s+=ord('z')-ord(c)
   return s
 
 def checkit(w):
@@ -24,9 +42,31 @@ def checkit(w):
 #    print (sumchar(w))
 
 
-with open("/usr/share/dict/french") as infile:
-#  checkit("corona")
-#  sys.exit(0)
-  for line in infile:
-    line=line.strip()
-    checkit(line)
+#with open("/usr/share/dict/french") as infile:
+##  checkit("corona")
+##  sys.exit(0)
+  #for line in infile:
+    #line=line.strip()
+    #checkit(line)
+
+
+
+# --------------------------------------------------------------------------
+def main():
+  phrase="Dave Ball".lower()
+  sfl=[sumchar1,sumchar0,revsumchar1,revsumchar0]
+  for sf in sfl:
+    st=0
+    print "--------"
+    for word in phrase.split():
+      s=sf(word)
+      st+=s
+      print(word+' '+str(s))
+    print(st)
+
+  
+
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()

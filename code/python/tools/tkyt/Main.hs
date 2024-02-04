2ee01ceda8449f6628602bb455b2b635774c1227
TK <thomas.kundera@yahoo.fr>
2024-02-04 20:34:27 +0100

    Compile

--- code/python/tools/tkyt/Main.hs
+++ code/python/tools/tkyt/Main.hs
@@ -1,5 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 import Data.Aeson
+import Data.Text (Text)
+import GHC.Generics
+
+data Person = Person { name :: Text, age :: Int } deriving Show
+
+data Ytpost = Ytpost {
+    cid :: Text,
+    text :: Text,
+    time :: Text,
+    author :: Text,
+    channel :: Text,
+    votes :: Int,
+    photo :: Text,
+    heart :: Text, -- Actually boolean
+    reply :: Text, -- Actually boolean
+    time_parsed :: Float
+} deriving Generic
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+        contents <- readFile "educative.txt"
+
+        putStrLn "Hello, Haskell!"

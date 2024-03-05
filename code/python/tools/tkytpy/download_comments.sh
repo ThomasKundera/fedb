dd447a477f94a1565bd34549e13df054608fb4d3
TK <thomas.kundera@yahoo.fr>
2024-03-05 16:08:05 +0100

    downloading better

--- code/python/tools/tkytpy/download_comments.sh
+++ code/python/tools/tkytpy/download_comments.sh
@@ -3,8 +3,14 @@ JSOND_DIR="json"
 
 mkdir -p $JSOND_DIR
 
-for yid in `cat ytvideo.dat`; do
+for yid in `cat ytvideos.dat`; do
     echo "Working with $yid"
     fout=$JSOND_DIR/${yid}.json
     echo "for $fout"
+    if ! [ -f $fout ]; then
+        echo 'File does not exists, generating'
+        youtube-comment-downloader -p --youtubeid $yid --output $fout
+    else
+        echo 'File does exist.'
+    fi
 done

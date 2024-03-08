7c02e0adf812a8609da276e5f675a88b8ea27943
TK <thomas.kundera@yahoo.fr>
2024-03-08 11:29:34 +0100

    starting customization

--- code/python/tools/tkytpy/tkytpyserver.py
+++ code/python/tools/tkytpy/tkytpyserver.py
@@ -6,9 +6,28 @@ PORT = 8000
 DIRECTORY = "/var/tkweb"
 
 
+
+
+
 class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=DIRECTORY, **kwargs)
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+  def do_GET(self):
+    if self.path == "/magic":
+      self.simple_magic()
+      return
+    super().do_GET()
+
+  def simple_magic(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+    self.wfile.write(bytes("<html><head><title>Magic!</title></head>", "utf-8"))
+    self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+    self.wfile.write(bytes("<body>", "utf-8"))
+    self.wfile.write(bytes("<p>This is an example.</p>", "utf-8"))
+    self.wfile.write(bytes("</body></html>", "utf-8"))
 
 
 def run():

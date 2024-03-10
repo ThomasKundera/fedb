1a504ae41af939f65dda6842193eb86aac6a05a9
TK <thomas.kundera@yahoo.fr>
2024-03-10 11:16:04 +0100

    Base class structure, using a glbal. Still do nothing

--- code/python/tools/tkytpy/tkytpyserver.py
+++ code/python/tools/tkytpy/tkytpyserver.py
@@ -1,36 +1,68 @@
 #!/usr/bin/env python3
 import http.server
 import socketserver
+import yattag
 
 PORT = 8000
 DIRECTORY = "/var/tkweb"
 
 
 
+class YTVideo:
+  def __init__(self,yid):
+    self.yid=yid
 
+
+class YTVideoList:
+  def __init__(self):
+    self.videos={}
+
+  def add(self,v):
+    self.videos[v.yid]=yid
+
+  def to_html(self,doc,tag,text,line):
+    for v in  self.videos:
+      with tag('p'):
+        text(v.yid)
+
+class TKYTGlobal:
+  def __init__(self):
+    self.videos=YTVideoList()
+    return
+
+  def return_page(self,path):
+    self.doc, self.tag, self.text, self.line = yattag.Doc().ttl()
+    self.doc.asis('<!DOCTYPE html>')
+    with self.tag('html'):
+      with self.tag('head'):
+        self.doc.stag('link',href='style.css',rel="stylesheet", type="text/css")
+        with self.tag('script', src='script.js'):
+          self.text()
+        with self.tag('title'):
+          self.text("YT comment manager - Main page")
+
+      with self.tag('body'):
+        with self.tag('h1'):
+          self.text("YT comment manager - Main page")
+
+      self.videos.to_html(self.doc, self.tag, self.text, self.line)
+
+    return (yattag.indent(self.doc.getvalue(), indent_text = True))
 
 class Handler(http.server.SimpleHTTPRequestHandler):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, directory=DIRECTORY, **kwargs)
 
   def do_GET(self):
-    if self.path == "/magic":
-      self.simple_magic()
-      return
-    super().do_GET()
-
-  def simple_magic(self):
     self.send_response(200)
     self.send_header("Content-type", "text/html")
     self.end_headers()
-    self.wfile.write(bytes("<html><head><title>Magic!</title></head>", "utf-8"))
-    self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-    self.wfile.write(bytes("<body>", "utf-8"))
-    self.wfile.write(bytes("<p>This is an example.</p>", "utf-8"))
-    self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    self.wfile.write(bytes(gtkyp.return_page(self.path), "utf-8"))
 
 
-def run():
+
+def runserver():
   with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
@@ -38,7 +70,9 @@ def run():
 
 # --------------------------------------------------------------------------
 def main():
-  run()
+  global gtkyp
+  gtkyp=TKYTGlobal()
+  runserver()
   
 # --------------------------------------------------------------------------
 if __name__ == '__main__':

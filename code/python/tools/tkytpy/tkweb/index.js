5bcc1f1699295852b02e840061aca48a93e980ce
TK <thomas.kundera@yahoo.fr>
2024-03-10 11:59:56 +0100

    Just tests

---
+++ code/python/tools/tkytpy/tkweb/index.js
@@ -0,0 +1,28 @@
+async function sendData(data) {
+  // Construct a FormData instance
+  const formData = new FormData();
+
+  // Add a text field
+  formData.append("name", "Pomegranate");
+
+  // Add a file
+  const selection = await window.showOpenFilePicker();
+  if (selection.length > 0) {
+    const file = await selection[0].getFile();
+    formData.append("file", file);
+  }
+
+  try {
+    const response = await fetch("https://example.org/post", {
+      method: "POST",
+      // Set the FormData instance as the request body
+      body: formData,
+    });
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const send = document.querySelector("#send");
+send.addEventListener("click", sendData);

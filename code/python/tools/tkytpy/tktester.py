f3b672d22f8c198349fb26c9dd0e1c4b67e35300
TK <thomas.kundera@yahoo.fr>
2024-03-15 13:37:05 +0100

    Look for // access to gglapi, but we wont

--- code/python/tools/tkytpy/tktester.py
+++ code/python/tools/tkytpy/tktester.py
@@ -13,6 +13,17 @@ sqluser='tkyt'
 sqlstring = sqlproto+'://'+sqluser+'@'+tksecrets.localsqldb_pass+'@localhost/tkyt'
 engine = create_engine(sqlstring, echo=True)
 
+# Create a new Http() object for every request
+def build_request(http, *args, **kwargs):
+  new_http = google_auth_httplib2.AuthorizedHttp(credentials, http=httplib2.Http())
+  return googleapiclient.http.HttpRequest(new_http, *args, **kwargs)
+authorized_http = google_auth_httplib2.AuthorizedHttp(credentials, http=httplib2.Http())
+service = discovery.build('api_name', 'api_version', requestBuilder=build_request, http=authorized_http)
+
+# Pass in a new Http() manually for every request
+service = discovery.build('api_name', 'api_version')
+http = google_auth_httplib2.AuthorizedHttp(credentials, http=httplib2.Http())
+service.stamps().list().execute(http=http)
 
 
 # --------------------------------------------------------------------------

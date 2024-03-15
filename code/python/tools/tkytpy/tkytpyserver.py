dad82c8479329aba3c14503870a35cddbf3d9be8
TK <thomas.kundera@yahoo.fr>
2024-03-15 13:38:06 +0100

    removing other non working stuff

--- code/python/tools/tkytpy/tkytpyserver.py
+++ code/python/tools/tkytpy/tkytpyserver.py
@@ -5,7 +5,6 @@ import socketserver
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-
 class HttpHandler(http.server.SimpleHTTPRequestHandler):
   DIRECTORY = "/var/tkweb"
   def __init__(self, *args, **kwargs):
@@ -20,16 +19,10 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
     post_data_bytes = self.rfile.read(content_length)
     logging.debug("HttpHandle:do_POST: Received data:\n", post_data_bytes)
 
-    post_data_str = post_data_bytes.decode("UTF-8")
-    js=json.loads(post_data_str)
-
-    gtkyp.add_video(YTVideo(js['ytid'],gtkyp))
-
-    jsr=json.dumps(gtkyp.get_video_dict())
     self.send_response(200)
     self.send_header('Content-type', 'application/json')
     self.end_headers()
-    self.wfile.write(jsr.encode(encoding='utf_8'))
+    self.wfile.write("{}".encode(encoding='utf_8'))
     logging.debug("HttpHandle:do_POST: END")
 
 def runserver():

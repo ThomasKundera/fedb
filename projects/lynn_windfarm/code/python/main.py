062e7f52dc3c3325ebbef84be74fee1f97a92364
Thomas Kundera <thomasked69@yahoo.fr>
2024-12-02 17:04:25 +0100

    Pretty cool

--- projects/lynn_windfarm/code/python/main.py
+++ projects/lynn_windfarm/code/python/main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-from matplotlib import pyplot as plt
+import os, sys
 
 import numpy as np
 
+from matplotlib import pyplot as plt
+from matplotlib.colors import hsv_to_rgb
+
 from skimage import data
-from skimage.color import rgba2rgb, rgb2hsv
+from skimage.color import rgba2rgb, rgb2hsv, rgb2gray
 from skimage.draw import ellipse
-from skimage.feature import corner_harris, corner_subpix, corner_peaks, match_template
+from skimage.feature import corner_harris, corner_subpix, corner_peaks, match_template, blob_dog, blob_log, blob_doh
 from skimage.measure import LineModelND, ransac
 from skimage.transform import warp, AffineTransform
 
@@ -30,18 +32,7 @@ dy = ly/py
 dxy = (dx+dy)/2.
 
 
-def object_identification():
-    # Open original jpeg image
-    original_image = plt.imread(os.path.join(
-        'data', '51664909026_2877f487d2_800.jpg'))
-    # Open data point image
-    data_point_image = plt.imread(os.path.join(
-        'data', '51664909026_2877f487d2_o_data_800.png'))
-
-    # select blue pixels only from data imagepoint image
-    rgb_data = rgba2rgb(data_point_image)
-    hsv_data = rgb2hsv(rgb_data)
-    hue_data = hsv_data[:, :, 0]
+def fit_horizon(hue_data):
     blue_data = hue_data > 0.5
 
     # Extract blue pixels from blue_data as a 2D array
@@ -63,19 +54,90 @@ def object_identification():
     # Fit a line to the data points
     model = LineModelND()
     model.estimate(data)
-    
+
     # generate coordinates of estimated models
     line_x = np.arange(0, 800)
     line_y = model.predict_y(line_x)
 
+    return line_x, line_y
+
+
+def plot_hsv(hsv_data):
+    rgb = hsv_to_rgb(hsv_data)
+    plt.imshow(rgb)
+    
+
+def find_windmills(hsv_data):
+    windmills = []
+    
+    # Extract white pixels from hsv_data as a 2D array
+    val_data = hsv_data[:, :, 2]
+    #plt.imshow(val_data)
+    #plt.show()
+    val_binary = val_data > 0.1
+    #plt.imshow(val_binary, cmap='gray')
+    #plt.show()
+    #sys.exit(0)
+    sat_data = hsv_data[:, :, 1]
+    #plt.imshow(sat_data)
+    #plt.show()
+    sat_binary = sat_data < 0.9
+    #plt.imshow(sat_binary, cmap='gray')
+    #plt.show()
+    #sys.exit(0)
+
+    white_data = np.logical_and(sat_binary, val_binary)
+    
+    #plt.imshow(white_data, cmap='gray')
+    #plt.show()
+    #sys.exit(0)
+
+    blobs_dog = blob_dog(white_data, min_sigma=0.001, threshold=0.0001)
+
+    for blob in blobs_dog:
+        print(blob)
+        windmills.append(blob)
+
+    return windmills
+
+
+def object_identification():
+    # Open original jpeg image
+    original_image = plt.imread(os.path.join(
+        'data', '51664909026_2877f487d2_o_detail.jpg'))
+    # Open data point image
+    data_point_image = plt.imread(os.path.join(
+        'data', '51664909026_2877f487d2_o_detail_data.png'))
+
+    # get hue data
+    rgb_data = rgba2rgb(data_point_image,background=(0, 0, 0))
+    hsv_data = rgb2hsv(rgb_data)
+
+    hue_data = hsv_data[:, :, 0]
+
+    # fit horizon
+    line_x, line_y = fit_horizon(hue_data)
+
+    # fit windmills
+    windmills = find_windmills(hsv_data)
+
     # Draw original image
     plt.imshow(original_image)
 
-    # Draw data point image
-    # plt.imshow(data_point_image, alpha=0.5)
-
     # Draw estimated line
     plt.plot(line_x, line_y)
+
+    # Draw windmills
+    for blob in windmills:
+        y, x, r = blob
+        c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
+        # plot circles
+        plt.gcf().gca().add_artist(c)
+
+    plt.tight_layout()
+    plt.show()
+
+
 
     plt.show()
 

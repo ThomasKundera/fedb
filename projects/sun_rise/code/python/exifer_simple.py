80cad2a7ecaf3098cc444f957494d11f3115552a
TK <thomas.kundera@yahoo.fr>
2025-08-31 09:41:54 +0200

    Using piexif

--- projects/sun_rise/code/python/exifer_simple.py
+++ projects/sun_rise/code/python/exifer_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+import math
+from PIL import Image
 import piexif
 
 # Print function with datestamp
@@ -28,26 +31,36 @@ class ExifData:
         if piexif.ExifIFD.FocalLength not in self.exif_dict['Exif']:
             logprint(f"WARNING: No focal length in EXIF for {self.imgfile}")
             return None
+
+        # Print image size:
+        image_width = self.exif_dict['0th'][piexif.ImageIFD.ImageWidth]
+        image_height = self.exif_dict['0th'][piexif.ImageIFD.ImageLength]
+        logprint(f"Image size: {image_width} x {image_height}")
+
         self.focal_length = self.exif_dict['Exif'][piexif.ExifIFD.FocalLength][0]
 
         if self.focal_length == 0:
             self.focal_length = 1012  # 1000mm f/10 + 13mm extension.
     
-    def px_to_angle(r, fl):
+    def px_to_angle(self, r):
         # Canon 550D: Sensor Size 22.3 x 14.9mm
         pxs = 22.3 / kRSD
         pys = pxs
         ps = (pxs + pys) / 2
-        a = 2 * math.atan2(r * ps, 2 * fl)  # Diameter in radians
+        a = 2 * math.atan2(r * ps, 2 * self.focal_length)  # Diameter in radians
         return a
 
-    def angle_to_px(a, fl):
+    def angle_to_px(self, a):
         """Convert angle  (radians) to pixel size for Canon 550D sensor."""
         pxs = 22.3 / kRSD  # Pixel size in mm (x-dimension)
         pys = pxs          # Assume same pixel size for y
         ps = (pxs + pys) / 2  # Average pixel size
-        r = (2 * fl * math.tan(a / 2)) / ps  # Pixel radius
+        r = (2 * self.focal_length * math.tan(a / 2)) / ps  # Pixel radius
         return r
+
+    def deg_to_px(self,a):
+        """Convert angular diameter (degrees) to pixel radius for Canon 550D sensor."""
+        return self.angle_to_px(math.radians(a))
 
 def main():
     exif_data = ExifData('data/2025_08_22/full_sun/IMG_1451.JPG')

a4d67572430ef9bfb5dffa93527f9ea7cf20ddcd
TK <thomas.kundera@yahoo.fr>
2026-04-17 07:24:03 +0200

    Cleaner

--- code/python/divers/ballistic.py
+++ code/python/divers/ballistic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Simulation of ballistic trajectory on 
@@ -15,14 +15,15 @@ import sys
 sys.path.insert(0, '../common')
 
 import math
-from tkunits import km, dg, pi, earth_radius
+from tkunits import m, km, dg, pi, g_earth
+
+def simple_free_fall(h):
+    t = math.sqrt(2 * h / g_earth)
+    return t  # Returns time in seconds as a float
 
 def main():
-  
-  
-  
-  
-  
+  print(simple_free_fall(400*m))
+
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
   main()

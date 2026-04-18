d3230d7247c3ae6a157ff6138804efe4829321ca
TK <thomas.kundera@yahoo.fr>
2026-04-18 19:03:30 +0200

    Trying to overlay for comparison

--- blender/iss/code/bash/run_render.sh
+++ blender/iss/code/bash/run_render.sh
@@ -22,5 +22,27 @@ cd $WORKDIR
 echo "Working in $WORKDIR"
 /usr/bin/nice -20 blender --python-use-system-env -b -P $PROJECTDIR/code/blender/iss_render.py
 #  -- --gpu-backend opengl
+echo "Render done"
 
+echo "Magiking"
+
+convert "$PROJECTDIR/output/renders/iss.png" \
+  -colorspace gray -alpha remove "$PROJECTDIR/output/iss-preproc.mpc"
+
+convert "$PROJECTDIR/output/iss-preproc.mpc" \
+  -morphology Convolve DoG:1,0,1 \
+  -threshold 20% \
+  -morphology Dilate Disk:1 \
+  -alpha set \
+  -alpha copy \
+  -channel a -negate +channel \
+  "png32:$PROJECTDIR/output/edges0.png"
+
+convert "$PROJECTDIR/output/edges0.png" \
+    -resize 4288x2848! \
+    "$PROJECTDIR/output/edges.png"
+
+convert $PROJECTDIR/org/255-STS-s130e012141.jpg $PROJECTDIR/output/edges.png \
+  -compose Over -composite \
+  $PROJECTDIR/output/comparison_edges.png
 echo "Done"
